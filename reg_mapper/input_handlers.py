087b779957dec4ee5c4039a0c8794ddb3631e770
--global <37449530+aappl@users.noreply.github.com>
2019-08-16 17:34:26 +0100

    Added function to call all input handlers and return all reg map dicts

--- reg_mapper/input_handlers.py
+++ reg_mapper/input_handlers.py
@@ -3,15 +3,21 @@ This file contains functions that convert input files into dictionaries. The dic
 be passed to the RegMapper class.
 """
 
+from pathlib import Path
 import json
+
+
 import cson
 
 input_handlers = []
 
 
 def json_handler(file_path):
+    """
+    Handles json file inputs. Takes Path object pointing to json file and returns
+    a python dictionary with the json files contents
+    """
     if file_path.suffix == '.json':
-        print("In json")
         with open(file_path, "r") as read_file:
             data = json.load(read_file)
             return data
@@ -21,8 +27,11 @@ input_handlers.append(json_handler)
 
 
 def cson_handler(file_path):
+    """
+    Handles cson file inputs. Takes Path object pointing to cson file and returns
+    a python dictionary with the cson files contents
+    """
     if file_path.suffix == '.cson':
-        print("In cson")
         with open(file_path, "r") as read_file:
             data = cson.load(read_file)
             return data
@@ -32,3 +41,17 @@ def cson_handler(file_path):
 
 
 input_handlers.append(cson_handler)
+
+
+def get_dicts(input_files):
+    """
+    Takes a list of input path strings and returns the dictionary contents.
+    """
+    input_dicts = []
+    for f in input_files:
+        for handler in input_handlers:
+            input_dict = handler(Path(f))
+            if input_dict:
+                input_dicts.append(input_dict)
+
+    return input_dicts
--- test/test_input_handlers.py
+++ test/test_input_handlers.py
@@ -3,10 +3,12 @@ from pathlib import Path
 from context import reg_mapper
 from reg_mapper import reg_mapper
 from reg_mapper.input_handlers import input_handlers
+from reg_mapper.input_handlers import get_dicts
 
 
 def test_input_handlers():
     import json
+
     test_file = Path("test/test.json")
 
     test_dicts = []
@@ -18,3 +20,20 @@ def test_input_handlers():
 
     assert test_dicts[0] == actual_dict
     assert test_dicts[1] is None
+
+
+def test_get_dicts():
+    import json
+    import cson
+
+    test_files = ["test/test.json", "test/test.cson"]
+    dicts = get_dicts(test_files)
+    print(dicts)
+
+    actual_dicts = []
+    with open(test_files[0], 'r') as f:
+        actual_dicts.append(json.load(f))
+    with open(test_files[1], 'r') as f:
+        actual_dicts.append(cson.load(f))
+
+    assert dicts == actual_dicts
